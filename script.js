07a21997523b0044468a1979ba45d89cdc36aee6
Miguel Joaquim <migueljoaquim409@gmail.com>
2025-04-22 20:16:28 -0300

    Aqui começa a nossa calculadora de gastos mensais; adicionado consts para selecionar formulário de despesas, lista de despesas e o elemento que vai ser exibido o total

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o formulário de despesas, a lista de despesas e o elemento que exibe o total
+const form = document.getElementById('formulario-gasto');
+const expenseList = document.getElementById('lista-gastos');
+const totalExpense = document.getElementById('total-gastos');
+
+// Array para armazenar as despesas
+let expenses = [];
